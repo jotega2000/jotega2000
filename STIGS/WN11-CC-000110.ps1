416c405c8ca6014487620178eb3618211596d14c
jotega2000 <65616151+jotega2000@users.noreply.github.com>
2026-04-07 23:27:17 -0400

    Update WN11-CC-000110.ps1

--- STIGS/WN11-CC-000110.ps1
+++ STIGS/WN11-CC-000110.ps1
@@ -6,8 +6,8 @@
     Author          : Julius Otega
     LinkedIn        : https://www.linkedin.com/in/julius-otega-17632913/
     GitHub          : https://github.com/jotega2000
-    Date Created    : 2024-04-06
-    Last Modified   : 2024-04-06
+    Date Created    : 2026-04-06
+    Last Modified   : 2026-04-06
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
